cc8748f7a36c1b5eb731fe33346f460b0e8677cf
[talk666] <[137696814@qq.com]>
2020-11-22 03:02:25 +0800

    init repo

---
+++ src/main/java/life/guolinxiang/community/CommunityApplication.java
@@ -0,0 +1,10 @@
+package life.guolinxiang.community;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityApplication {
+    public static void main(String[] args) { SpringApplication.run(CommunityApplication.class,args);
+
+    }
+}
